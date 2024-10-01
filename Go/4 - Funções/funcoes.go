f433dc0d45b0f8a544d191b2cc111f1a421f11d3
Lucas Minosso Reis <lucasminossoreis@gmail.com>
2024-09-30 21:40:52 -0300

    Aprendendo Funções

---
+++ Go/4 - Funções/funcoes.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func somar(n1 int8, n2 int8) int8 {
+	return n1 + n2
+}
+
+func main() {
+	soma := somar(10, 20)
+	fmt.Println(soma)
+
+	var f = func(txt string) string {
+		fmt.Println(txt)
+		return txt
+	}
+
+	resultado := f("Texto da Função 1")
+	fmt.Println(resultado)
+}
