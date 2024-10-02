bf60291df689753f7a71d832622c691b2bd42c25
Lucas Minosso Reis <lucasminossoreis@gmail.com>
2024-10-01 22:01:41 -0300

    Aprendendo Funções

--- Go/4 - Funções/funcoes.go
+++ Go/4 - Funções/funcoes.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
-func somar(n1 int8, n2 int8) int8 {
+func somar(n1, n2 int8) int8 { // Se variável for do mesmo tipo, pode informar apenas no último.
 	return n1 + n2
+}
+
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
 }
 
 func main() {
@@ -11,10 +17,13 @@ func main() {
 	fmt.Println(soma)
 
 	var f = func(txt string) string {
-		fmt.Println(txt)
+		println(txt)
 		return txt
 	}
 
 	resultado := f("Texto da Função 1")
 	fmt.Println(resultado)
+
+	resultadoSoma, _ := calculosMatematicos(10, 15) //Utilizar o underline pode esconder a variável.
+	fmt.Println(resultadoSoma)
 }
