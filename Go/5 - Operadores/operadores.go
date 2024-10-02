376677d1f9ad602bdf7c2b1810e3214d2de8ca4e
Lucas Minosso Reis <lucasminossoreis@gmail.com>
2024-10-01 22:30:27 -0300

    Aprendendo Operadores
    Aritméticos

---
+++ Go/5 - Operadores/operadores.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Aritmeticos
+	soma := 1 + 2
+	subtracao := 1 - 2
+	divisao := 10 / 4
+	multiplicacao := 3 * 3
+	restoDaDivisao := 10 % 2
+
+	fmt.Println(soma, subtracao, divisao, multiplicacao, restoDaDivisao)
+
+	var numero1 int16 = 10
+	var numero2 int16 = 25
+
+	var soma2 int16 = numero1 + numero2
+	soma3 := numero1 + numero2
+	fmt.Println(soma2)
+	fmt.Println(soma3)
+
+	// Fim dos Aritmeticos
+}
