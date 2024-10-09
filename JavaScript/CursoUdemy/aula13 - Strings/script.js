fbd05e65ec2ef2b9abfe70f5b4047f21d464a0c9
Lucas Minosso Reis <lucasminossoreis@gmail.com>
2024-10-08 22:38:07 -0300

    Mais sobre strings

---
+++ JavaScript/CursoUdemy/aula13 - Strings/script.js
@@ -0,0 +1,9 @@
+// Coisas possíveis de fazer com string
+
+let umaString = "O rato roeu a roupa do rei de roma."
+
+console.log(umaString.replace('rato', "gato"))
+console.log(umaString.toLowerCase())
+
+// Após chamar a string, a tecla espaço mostra todas as possibilidades de execução.
+// Também é possível consultar a documentação em W3 Schools ou MDN ou Ecma
