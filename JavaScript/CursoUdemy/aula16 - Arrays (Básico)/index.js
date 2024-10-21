54de5aaf95f6353f5f87a5bf57ee519fd0b89660
Lucas Minosso Reis <lucasminossoreis@gmail.com>
2024-10-20 22:22:06 -0300

    Arrays Básico Finalizado

--- JavaScript/CursoUdemy/aula16 - Arrays (Básico)/index.js
+++ JavaScript/CursoUdemy/aula16 - Arrays (Básico)/index.js
@@ -1,1 +1,50 @@
-const alunos = [`Luiz`, `Maria`, `João`]
+//               0       1        2       --> indexado
+const alunos = ['Luiz', 'Maria', 'João']
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+/*
+alunos.push('Luiza')
+alunos.push('Eduardo')
+// console.log(alunos.slice(0, 3)) // até João
+console.log(alunos.slice(0, -1)) // Pega o total da array -1
+*/
+
+/*
+console.log(alunos[50]) 
+*/
+
+/*
+delete alunos[1]               --> //Remove mas mantém indice vazio/undefined
+console.log(alunos[1])
+*/
+
+/*
+const removido = alunos.shift()       // remove do início e .pop remove do final
+console.log(removido)
+*/
+
+/*
+alunos.unshift('Luiza')    // Adiciona no começo
+alunos.unshift('Fábio')
+*/
+
+/* alunos.push('Luiza')        /Adiciona no fim
+alunos.push('Fábio')
+*/
+
+/*
+alunos[alunos.length] = 'Luiza'    // Adiciona no fim
+alunos[alunos.length] = 'Fábio'
+alunos[alunos.length] = 'Luana'
+*/
+
+/*alunos[0] = 'Eduardo'    //Altera
+alunos[3] = 'Luiza '       //Altera
+*/
+
+/* console.log(alunos)
+console.log(alunos[0])
+console.log(alunos[2])
+*/ 
