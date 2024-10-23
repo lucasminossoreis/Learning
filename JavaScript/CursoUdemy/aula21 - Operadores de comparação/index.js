822be92eb84b42401e4943e3a1391a3d921c9606
Lucas Minosso Reis <lucasminossoreis@gmail.com>
2024-10-22 21:56:22 -0300

    Operadores de Comparação e Lógicos

---
+++ JavaScript/CursoUdemy/aula21 - Operadores de comparação/index.js
@@ -0,0 +1,19 @@
+/*
+Operadores de Comparação
+> Maior que
+>= Maior ou igual a
+< Menor que
+<= Menor ou igual a
+== igualdade (valor)   ******* não recomendado utilizar
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)   ******* não recomendado utilizar
+!== diferente estrito (valor e tipo)
+*/
+
+const compar = 10 > 5;
+console.log(compar);
+
+const testar1 = 10
+const testar2 = 20
+const testar3 = testar1 === testar2
+console.log(testar3)
---
+++ JavaScript/CursoUdemy/aula22 - Operadores Lógicos/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores Lógicos
+&& --> AND --> E     --> Todas as expressões precisam ser verdadeiras para retornar true
+|| --> OR --> OU     --> Apenas uma expressão precisa ser verdadeira para retornar true
+!  --> NOT --> NÃO   --> 
+*/
+
+const expressaoAND = true && true && true;
+console.log(expressaoAND);
+
+const expressaoOR = true || false || true
+console.log(expressaoOR)
+
+console.log(!true)
