98e8dea8acee49975947e06324666ea4d0d5c940
Lucas Minosso Reis <lucasminossoreis@gmail.com>
2024-11-25 22:33:33 -0300

    Aprendendo C/C++

---
+++ CeCPlusPlus/C/aula01.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+    int val1 = 10;
+    float val2 = 2.5;
+    char nome[20]= "Lucas";
+    printf("\n O valor da variavel e: %d e do valor 2 e: %.2f", val1, val2);
+    printf("\n O nome e: %s", nome);
+
+    return 0;
+}
---
+++ CeCPlusPlus/C/aula02.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<locale.h>
+
+int main(void)
+{
+    char sexo;
+    char nome[30];
+    float nota1, nota2;
+    setlocale(0, "portuguese");
+    printf("\nDigite o nome do aluno:");
+    fflush(stdin);
+    gets(nome);
+    printf("\nInforme o sexo (F/M):");
+    fflush(stdin);
+    sexo=getchar();
+    printf("\nInforme a nota 1:");
+    scanf("%f", &nota1);
+    printf("\nInforme a nota 2:");
+    scanf("%f", &nota2);
+    printf("\n%s é do sexo %c possui  media = %2.f", nome, sexo, (nota1+nota2)/2);
+    
+    return 0;
+}
